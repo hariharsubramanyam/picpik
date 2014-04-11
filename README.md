7b4688449d3c5f60082140c3ded69771e5c46831
Harihar Subramanyam <hsubrama@mit.edu>
2014-04-10 21:06:41 -0400

    PicPik Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Picture Picker
+
+## Usage
+
+Start the server:
+
+`npm start`
+
+Navigate your browser to:
+
+`localhost:8000/app/`
+
---
+++ app/js/controllers.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('MyCtrl1', ["$scope", function($scope) {
+  	$scope.pics = [];
+    $scope.selected_pics = {};
+  	var num_pics_registered = 0;
+  	for(var i = 1; i <=50; i++){
+  		$scope.pics.push("../assets/pictures/pic"+i+".jpg");
+      $scope.selected_pics[$scope.pics[i]] = false;
+  	}
+    $scope.init = function(){
+    	num_pics_registered += 1;
+    	if(num_pics_registered == $scope.pics.length){
+    		$('.gridly').gridly({
+	      		base: 60, // px
+	      		gutter: 20, // px
+	      		columns: 10
+	    	});
+    	}
+    };
+    $scope.on_pick_click = function(pic_name){
+      $scope.selected_pics[pic_name] = true;
+    };
+
+  }]);
