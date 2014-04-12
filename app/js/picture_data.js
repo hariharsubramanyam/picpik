a0464113465d508702b05caf13474cb96e37ebd5
Harihar Subramanyam <hsubrama@mit.edu>
2014-04-11 20:53:32 -0400

    Data file

---
+++ app/js/picture_data.js
@@ -0,0 +1,189 @@
+var pic_data = [
+	{
+		group_name: "France",
+		pictures: [
+			{
+				src: "../assets/pictures/pic1.jpg"
+			},
+			{
+				src: "../assets/pictures/pic2.jpg"
+			},
+			{
+				src: "../assets/pictures/pic3.jpg"
+			},
+			{
+				src: "../assets/pictures/pic4.jpg"
+			},
+			{
+				src: "../assets/pictures/pic5.jpg"
+			},
+			{
+				src: "../assets/pictures/pic6.jpg"
+			},
+			{
+				src: "../assets/pictures/pic7.jpg"
+			},
+			{
+				src: "../assets/pictures/pic8.jpg"
+			},
+			{
+				src: "../assets/pictures/pic9.jpg"
+			},
+			{
+				src: "../assets/pictures/pic10.jpg"
+			},
+			{
+				src: "../assets/pictures/pic11.jpg"
+			},
+			{
+				src: "../assets/pictures/pic12.jpg"
+			},
+			{
+				src: "../assets/pictures/pic13.jpg"
+			},
+			{
+				src: "../assets/pictures/pic14.jpg"
+			},
+		],
+		subgroups:[
+			{
+				group_name: "Paris",
+				pictures: [
+					{
+						src: "../assets/pictures/pic15.jpg"
+					},
+					{
+						src: "../assets/pictures/pic16.jpg"
+					},
+					{
+						src: "../assets/pictures/pic17.jpg"
+					},
+					{
+						src: "../assets/pictures/pic18.jpg"
+					},
+					{
+						src: "../assets/pictures/pic19.jpg"
+					},
+					{
+						src: "../assets/pictures/pic20.jpg"
+					},
+					{
+						src: "../assets/pictures/pic21.jpg"
+					},
+				]
+			},
+			{
+				group_name: "Marseille",
+				pictures: [
+					{
+						src: "../assets/pictures/pic22.jpg"
+					},
+					{
+						src: "../assets/pictures/pic23.jpg"
+					},
+					{
+						src: "../assets/pictures/pic24.jpg"
+					},
+					{
+						src: "../assets/pictures/pic25.jpg"
+					},
+					{
+						src: "../assets/pictures/pic26.jpg"
+					},
+					{
+						src: "../assets/pictures/pic27.jpg"
+					},
+					{
+						src: "../assets/pictures/pic28.jpg"
+					},
+				]
+			}
+		]
+	},
+	{
+		group_name: "Spain",
+		pictures: [
+			{
+				src: "../assets/pictures/pic28.jpg"
+			},
+			{
+				src: "../assets/pictures/pic29.jpg"
+			},
+			{
+				src: "../assets/pictures/pic30.jpg"
+			},
+			{
+				src: "../assets/pictures/pic31.jpg"
+			},
+			{
+				src: "../assets/pictures/pic32.jpg"
+			},
+			{
+				src: "../assets/pictures/pic33.jpg"
+			},
+			{
+				src: "../assets/pictures/pic34.jpg"
+			},
+			{
+				src: "../assets/pictures/pic35.jpg"
+			},
+			{
+				src: "../assets/pictures/pic36.jpg"
+			},
+			{
+				src: "../assets/pictures/pic37.jpg"
+			},
+			{
+				src: "../assets/pictures/pic38.jpg"
+			},
+			{
+				src: "../assets/pictures/pic39.jpg"
+			},
+			{
+				src: "../assets/pictures/pic40.jpg"
+			},
+			{
+				src: "../assets/pictures/pic41.jpg"
+			},
+		],
+		subgroups:[
+			{
+				group_name: "Madrid",
+				pictures: [
+					{
+						src: "../assets/pictures/pic42.jpg"
+					},
+					{
+						src: "../assets/pictures/pic43.jpg"
+					},
+					{
+						src: "../assets/pictures/pic44.jpg"
+					},
+					{
+						src: "../assets/pictures/pic45.jpg"
+					},
+					{
+						src: "../assets/pictures/pic46.jpg"
+					},
+					{
+						src: "../assets/pictures/pic47.jpg"
+					},
+					{
+						src: "../assets/pictures/pic48.jpg"
+					},
+				]
+			},
+			{
+				group_name: "Barcelona",
+				pictures: [
+					{
+						src: "../assets/pictures/pic49.jpg"
+					},
+					{
+						src: "../assets/pictures/pic50.jpg"
+					},
+				]
+			}
+		]
+	}
+];
