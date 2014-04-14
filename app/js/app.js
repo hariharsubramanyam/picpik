d2852e93e798cae92e355ee974fb633a363b8f15
Harihar Subramanyam <hsubrama@mit.edu>
2014-04-14 13:54:12 -0400

    Removed some useless stuff

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,5 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives',
   'myApp.controllers'
 ]);
