ac2a8c3dab6ba04b1bfe7ca3f28046618f111c1f
Lars Johnson <larsj@mit.edu>
2014-05-09 07:34:14 -0400

    Basic empty drop targets for groups

---
+++ app/js/app/views/pictarget.js
@@ -0,0 +1,39 @@
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'text!templates/pictarget.html',
+    'collections/tagset',    
+    'common',
+    'models/undomanager'
+], function($, _, Backbone, picTargetTemplate, TagSet, Common, UndoManager) {
+    /**
+     * The View object for a Picture in the grid.
+     * The view object is a div
+     */
+    var PicTargetView = Backbone.View.extend({
+        tagName: "div",
+        
+        template: _.template(picTargetTemplate),
+        
+        events: {
+            "mousedown  .thumb" : "mouseDown",
+            "mouseup  .thumb" : "mouseUp",
+            "mousemove  .thumb" : "mouseMove",
+            "dblclick  .thumb" : "doubleClick",
+            "click .favorite" : "favorite",
+            "click .unfavorite" : "unfavorite",
+        },
+        
+        initialize: function() {
+            this.listenTo(this, 'destroy', this.remove);
+            this.partialClick = false;
+        },
+        
+        render: function() {
+            this.$el.addClass("pic_target");                             
+            return this;
+        },
+    });
+    return PicTargetView;
+});
